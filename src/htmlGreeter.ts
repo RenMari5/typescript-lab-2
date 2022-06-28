31bdf4464ada32880b587cf318d4a4ef453fc430
Ren <rpaczkowski@gmail.com>
2022-06-28 14:26:38 -0400

    Refactored the javascript and HTML greeters

--- src/htmlGreeter.ts
+++ src/htmlGreeter.ts
@@ -3,12 +3,12 @@ import { Greeter } from "./greeter";
 export class HtmlGreeter extends Greeter {
   tagName: string;
 
-  constructor(greeting: string, tagName: string) {
+  constructor(greeting: string, tagName: string = "h1") {
     super(greeting);
     this.tagName = tagName;
   }
 
   greet(name: string): any {
-    return `<h1>${this.greeting}, ${name}!</h1>`;
+    return `<${this.tagName}>${super.greet(name)}</${this.tagName}>`;
   }
 }
--- src/javascriptGreeter.ts
+++ src/javascriptGreeter.ts
@@ -2,6 +2,6 @@ import { Greeter } from "./greeter";
 
 export class JavaScriptGreeter extends Greeter {
   greet(name: string): any {
-    return `console.log((${this.greeting}, ${name}!))`;
+    return `console.log(${super.greet(name)})`;
   }
 }
